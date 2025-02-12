fae8d67633ca6ae2aab6bab577c4caf1639853a8
Vietnguyen2601 <thuybinh2612004@gmail.com>
2025-02-12 13:34:28 +0700

    BackendCodingForm

---
+++ ChildVaccinationTrackingSystem/src/main/java/swp/ChildVaccinationTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package swp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildVaccinationTrackingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildVaccinationTrackingSystemApplication.class, args);
+    }
+
+}
---
+++ ChildVaccinationTrackingSystem/src/test/java/swp/ChildVaccinationTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package swp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildVaccinationTrackingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
